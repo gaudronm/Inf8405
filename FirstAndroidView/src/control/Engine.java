34434abe0c3be2f2c1186f33b6633977d6bd4e2b
Math <mathieu.gaudron@phelma.grenoble-inp.fr>
2014-02-15 16:52:03 -0500

    ajout de ValidMove et canCombinate

--- FirstAndroidView/src/control/Engine.java
+++ FirstAndroidView/src/control/Engine.java
@@ -98,5 +98,44 @@ public class Engine {
 	private int GenColAl() {		
 		return (int)Math.random()*nbColors;
 	}
+	
+	private boolean ValidMove(int x, int y, int i, int j) {
+		if (((x != i) && (y != j)) || ((Math.abs(x-i) != 1) && (Math.abs(y-j) != 1))) {
+			return false;
+		}
+		if ((canCombinate(x, y, table.getColor(i, j))) || (canCombinate(i, j, table.getColor(x, y)))) {
+			return true;
+		}
+		return false;		
+	}
+
+	
+	private boolean canCombinate(int x, int y, int color) {
+		if ((x - 1 >= 0) && (table.getColor(x - 1, y) == color)) {
+			if ((x - 2 >= 0) && (table.getColor(x - 2, y) == color)) {
+				return true;
+			} else if ((x + 1 <= 7) && (table.getColor(x + 1, y) == color)) {
+				return true;
+			}
+		}
+		if ((x + 1 <= 7) && (table.getColor(x + 1, y) == color)) {
+			if ((x + 2 <= 7) && (table.getColor(x + 2, y) == color)) {
+				return true;
+			}
+		}
+		if ((y - 1 >= 0) && (table.getColor(x, y - 1) == color)) {
+			if ((y - 2 >= 0) && (table.getColor(x, y - 2) == color)) {
+				return true;
+			} else if ((y + 1 <= 7) && (table.getColor(x, y + 1) == color)) {
+				return true;
+			}
+		}
+		if ((y + 1 <= 7) && (table.getColor(x, y + 1) == color)) {
+			if ((y + 2 <= 7) && (table.getColor(x, y + 2) == color)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
