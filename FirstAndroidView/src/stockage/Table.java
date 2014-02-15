767d283038729ca97c12ccc3e2fdc4f128d6c41b
Math <mathieu.gaudron@phelma.grenoble-inp.fr>
2014-02-15 13:58:08 -0500

    classe de stockage de la grille

---
+++ FirstAndroidView/src/stockage/Table.java
@@ -0,0 +1,22 @@
+package stockage;
+
+public class Table {
+
+	int table[][];
+
+	public Table() {
+		this.table = new int[8][8];
+	}
+
+	public int getColor(int x,int y) {
+		return table[x][y];
+	}
+
+	public void setColor(int x, int y, int c) {
+		this.table[x][y] = c;
+	}
+	
+	
+	
+	
+}
